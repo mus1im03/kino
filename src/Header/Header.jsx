6d7441dbf837f9b41ad5d5a15951b6716be03d6b
Muslim Bariev <muslim@MacBook-Air-Muslim.local>
2023-11-20 15:21:15 +0300

    docinul slice

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import styles from './Header.module.css';
 import logo from '../assets/img/logo.png'
+import './Header.css'
 
 const Header = () => {
     return (
         <header>
-            <div className="header_block">
-                <div className="img_block">
+            <div className={styles.header_block}>
+                <div className={styles.img_block}>
                     <img src={logo} alt="logo" />
+                </div>
+                <ul className={styles.categories}>
+                    <li href="" className='nav'>Главная</li>
+                    <li href="" className='nav'>Жанры</li>
+                    <li href="" className='nav'>Фильмы</li>
+                    <li href="" className='nav'>Сериалы</li>
+                    <li href="" className='nav'>Мультфильмы</li>
+                </ul>
+                <div className={styles.right_block}>
+                    <input type="text" id="" />
+                    <button>ВОЙТИ</button>
                 </div>
             </div>
         </header>

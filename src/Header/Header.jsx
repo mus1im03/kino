1fec3ce51720a76c8d67d4c9b2778d8517288acc
Muslim Bariev <muslim@MacBook-Air-Muslim.local>
2023-11-20 14:33:31 +0300

    sdelal

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import styles from './Header.module.css';
-import logo from '../assets/img/reposition_iviLogoPlateRounded.svg'
+import logo from '../assets/img/logo.png'
 
 const Header = () => {
     return (
         <header>
             <div className="header_block">
                 <div className="img_block">
-                    <img src={logo} alt="" />
+                    <img src={logo} alt="logo" />
                 </div>
             </div>
         </header>

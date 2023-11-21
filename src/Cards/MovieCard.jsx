c4aa8a4f83365b0378982d8ab2a488af49ef7f84
Шамхан Товсултанов <shamkhan.tvs@mail.ru>
2023-11-21 11:05:15 +0300

    qwerty

--- src/Cards/MovieCard.jsx
+++ src/Cards/MovieCard.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import './MovieCard.module.css';
 
 const MovieCard = ({ title, year, image }) => {
   return (
-    <div>
-      <img src={image} alt={title}/>
-      <div>
-        <h2>{title}</h2>
-        <p>{year}</p>
+    <div className="movie-card">
+      <img src='https://img.freepik.com/free-photo/a-painting-of-a-mountain-lake-with-a-mountain-in-the-background_188544-9126.jpg?size=626&ext=jpg' alt={title} />
+      <div className="movie-details">
+        <h2>{title}aaa</h2>
+        <p>{year}bbb</p>
       </div>
     </div>
   );
 };
 
 export default MovieCard;
+
--- src/Cards/MovieList.jsx
+++ src/Cards/MovieList.jsx
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, { useEffect } from "react";
 
-import MovieCard from './MovieCard';
-import { useSelector } from 'react-redux';
+import MovieCard from "./MovieCard";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchFilms } from "../features/filmSlice";
+import styles from "../Cards/MovieList.module.css";
 
 const MovieList = () => {
-
-    const films = useSelector((state) => state.films.films )
-
+  const films = useSelector((state) => state.films.films);
+  console.log(films);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchFilms());
+  }, [dispatch]);
   return (
-    <div>
-      {films.map((film, i) => {
+    <div className={styles.cards}>
+      {films.map((film) => {
         return (
-          <MovieCard
-           key={film._id}
-           img={film.image}
-           title={film.title}
-           year={film.year}
-          />
+          <div className={styles.img}>
+            <p>{film.name}</p>
+            <p>{film.years}</p>
+          </div>
         );
       })}
     </div>
   );
- };
+};
 
-export default MovieList
+export default MovieList;

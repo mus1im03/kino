be134adb7b448af656dcc123754392133d791460
Halifat <hanif.001@mail.ru>
2023-11-21 16:21:01 +0300

    3 исправление

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,15 @@
-<<<<<<< HEAD
 import React from "react";
 import Header from "./Header/Header";
 import "../src/App.css";
 import { Route, Router } from "react-router-dom";
-=======
-import React from 'react';
-import Header from './Header/Header';
-import '../src/App.css'
-import FilmBody from './Body_film_page/FilmBody';
->>>>>>> main
+
 
 const App = () => {
   return (
-<<<<<<< HEAD
     <>
       <div>
         <Header />
+      
       </div>
 
       {/* Роуты тут */}
@@ -23,12 +17,7 @@ const App = () => {
         <Route path="/genre/:genreId" element={<Products />} />
       </Router> */}
     </>
-=======
-    <div>
-      <Header />
-      <FilmBody/>
-    </div>
->>>>>>> main
+   
   );
 };
 

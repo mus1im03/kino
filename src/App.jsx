536432598d827f7d796fe51090fcb3f8e21efe69
Muslim Bariev <muslim@MacBook-Air-Muslim.local>
2023-11-21 13:32:54 +0300

    dodelal genres

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import Header from './Header/Header';
-import '../src/App.css'
+import React from "react";
+import Header from "./Header/Header";
+import "../src/App.css";
+import { Route, Router } from "react-router-dom";
 
 const App = () => {
-
   return (
-    <div>
-      <Header />
-    </div>
+    <>
+      <div>
+        <Header />
+      </div>
+
+      {/* Роуты тут */}
+      {/* <Router>
+        <Route path="/genre/:genreId" element={<Products />} />
+      </Router> */}
+    </>
   );
 };
 
